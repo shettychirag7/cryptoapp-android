9bce33bbe3eda694d2f784e19f49e6cc82cbe900
shettychirag7 <shettychirag7@gmail.com>
2023-05-23 01:28:27 +0800

    chore(setup): base project setup with jetpack compose

---
+++ app/src/main/java/com/chiragshetty/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.chiragshetty.cryptocurrencyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.chiragshetty.cryptocurrencyapp.ui.theme.CryptocurrencyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
